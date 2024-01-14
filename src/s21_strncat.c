201012500397a3157a58acb351701fd457726034
Vartent <yabloko3000@gmail.com>
2024-01-14 13:34:59 +0300

    fix huyeviy code fokie shel nah loh

--- src/s21_strncat.c
+++ src/s21_strncat.c
@@ -5,7 +5,7 @@ char *s21_strncat(char *dest, const char *source, s21_size_t n) {
   s21_size_t sourceLength = s21_strlen(source);
 
   if (sourceLength < n) {
-    s21_strncpy(&dest[destLength], source, sourceLength);
+    s21_strncpy(&dest[destLength], source, n);
   } else {
     s21_strncpy(&dest[destLength], source, n);
     dest[destLength + n] = '\0';
